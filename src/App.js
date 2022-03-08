058f148eb9780d1332b87eb01f59391d528a6eac
Kristian Wobbes <kristian.wobbes@gmail.com>
2022-03-08 12:08:15 +0100

    finished task 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ import CoffeeControl from './components/CoffeeControl';
 import CoffeeInfo from './components/CoffeeInfo';
 import Thermos from './components/Thermos';
 
-function App() {
-  const coffee = { brewedAt: '2022-02-28T09:17:57.652Z', litersBrewed: '1.5' };
-  return (
-    <div className="App">
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+
+    this.state = {
+      litersBrewed: "1.5",
+      onClick: this.handleClick
+    }
+  }
+
+  handleClick(newLiters) {
+    //console.log("Hello")
+    //console.log(newLiters)
+    this.setState( {
+      litersBrewed: newLiters
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header></header>
       <main>
 
         <div id="examples">
           <h1>Examples</h1>
-          <Thermos {...coffee} />
+          <Thermos {...this.state} />
 
-          {/* <Thermos {...coffee} className="neumorphism-card"/> */}
+          <Thermos {...this.state} className="neumorphism-card"/>
 
-          <CoffeeInfo {...coffee} />
+          <CoffeeInfo {...this.state} />
 
           <CoffeeButton />
 
@@ -30,13 +51,13 @@ function App() {
 
         <span>This is Thermos and CoffeeInfo</span>
         <section id="top-container" className="container neumorphism-card">
-          <Thermos {...coffee} />
-          <CoffeeInfo {...coffee} />
+          <Thermos {...this.state} />
+          <CoffeeInfo {...this.state} />
         </section>
 
         <span>This is Thermos and CoffeeButtons (individual components)</span>
         <section id="prepare-coffee-container" className="container neumorphism-card">
-          <Thermos {...coffee} />
+          <Thermos {...this.state} />
 
           <div className="flex-columns">
             <CoffeeButton litersToBrew='2.1' />
@@ -51,12 +72,11 @@ function App() {
 
         <span>This is CoffeeControl (composition: Thermos and CoffeeButton(s)))</span>
         <section>
-          <CoffeeControl {...coffee} />
+          <CoffeeControl {...this.state} />
         </section>
 
       </main>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
